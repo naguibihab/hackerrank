6e4112664b4eda0b9e4e5f323c8b4c6cc87b28f9
Nick Ihab <n.ihab@learnmeter.com>
2017-12-08 15:51:07 +1100

    Method to calculate heading 1

--- queens-attack-2/main.js
+++ queens-attack-2/main.js
@@ -24,14 +24,56 @@ function main() {
     var n_temp = readLine().split(' ');
     var n = parseInt(n_temp[0]);
     var k = parseInt(n_temp[1]);
-    var rQueen_temp = readLine().split(' ');
-    var rQueen = parseInt(rQueen_temp[0]);
-    var cQueen = parseInt(rQueen_temp[1]);
+    var queen.rueen_temp = readLine().split(' ');
+    var queen.rueen = parseInt(queen.rueen_temp[0]);
+    var queen.cueen = parseInt(queen.rueen_temp[1]);
     for(var a0 = 0; a0 < k; a0++){
         var rObstacle_temp = readLine().split(' ');
         var rObstacle = parseInt(rObstacle_temp[0]);
         var cObstacle = parseInt(rObstacle_temp[1]);
         // your code goes here
+        
     }
+    
+    
+}
 
+function calcDistanceToQueen(xpoint,queen){
+
+}
+
+// There's an easier way to get the heading by splitting it into two headings
+function getHeading(xpoint,queen){
+    // North
+    if(xpoint.r > queen.r && xpoint.c == queen.c){
+        return 'N';
+    }
+    // North East
+    else if(xpoint.r > queen.r && xpoint.c > queen.c){
+        return 'NE';
+    }
+    // East
+    else if(xpoint.r == queen.r && xpoint.c > queen.c){
+        return 'E';
+    }
+    // South East
+    else if(xpoint.r < queen.r && xpoint.c < queen.c){
+        return 'SE';
+    }
+    // South
+    else if(xpoint.r < queen.r && xpoint.c == queen.c){
+        return 'S';
+    }
+    // South West
+    else if(xpoint.r < queen.r && xpoint.c < queen.c){
+        return 'SW';
+    }
+    // West
+    else if(xpoint.r == queen.r && xpoint.c < queen.c){
+        return 'W';
+    }
+    // North West
+    else if(xpoint.r > queen.r && xpoint.c < queen.c) {
+        return 'NW';
+    }
 }
