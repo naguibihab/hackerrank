0ae5ddd8742e616c72191f7e5aa2857a1f8c18bb
Nick Ihab <n.ihab@learnmeter.com>
2017-12-15 10:45:05 +1100

    Calculating distance to board but it's not correct

--- queens-attack-2/main.js
+++ queens-attack-2/main.js
@@ -31,6 +31,8 @@ function main() {
                 r: rQueen,
                 c: cQueen
             };
+    var allHeadings = ['N','NE','E','SE','S','SW','W','NW'];
+    var totalDistance = 0;
     var obstaclesDistance = [];
     for(var a0 = 0; a0 < k; a0++){
         var rObstacle_temp = readLine().split(' ');
@@ -45,16 +47,80 @@ function main() {
         var obstacleHeading = getHeading(queen,obstacle);
         if(obstacleHeading != -1){
             var obstacleDistance = calcDistanceToQueen(queen,obstacle,obstacleHeading);
-            //obstacles[obstacleHeading] = obstacleDistance;
+            obstaclesDistance[obstacleHeading] = obstacleDistance;
+            totalDistance += obstacleDistance; // This needs to change later in case two obstacles are in the same heading
         }
     }
     
+    // Calc distance from queen to board where there are no obstacles
+    var boardNE = {
+        r: n,
+        c: n
+    };
+    var boardSE = {
+        r: 1,
+        c: n
+    };
+    var boardSW = {
+        r: 1,
+        c: 1
+    };
+    var boardNW = {
+        r: n,
+        c: 1
+    };
     
+    if(!('N' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardNE,'N');
+    }
+    if(!('NE' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardNE,'NE');
+    }
+    if(!('E' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardNE,'E');
+    }
+    if(!('SE' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardSE,'SE');
+    }
+    if(!('S' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardSE,'S');
+    }
+    if(!('SW' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardSW,'SW');
+    }
+    if(!('W' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardSW,'W');
+    }
+    if(!('NW' in obstaclesDistance)){
+        totalDistance+= calcDistanceToQueen(queen,boardNW,'NW');
+    }
+    
+    console.log(totalDistance);
+//    process.stdout.write(totalDistance);
 }
 
 function calcDistanceToQueen(queen,xpoint,heading){
-    console.log(xpoint);
-    process.stdout.write(heading+'\n');
+    var distance = 0;
+    switch(heading[0]){
+        case 'N':
+            distance += xpoint.r - queen.r;
+            break;
+        case 'S':
+            distance += queen.r - xpoint.r;
+            break;
+    }
+    
+    switch(heading[1]){
+        case 'E':
+            distance += xpoint.c - queen.c;
+            break;
+        case 'W':
+            distance += queen.c - xpoint.c;
+            break;
+    }
+    
+    console.log('calcDistance',xpoint,heading[0],heading[1],distance);
+    return distance;
 }
 
 function getHeading(queen,xpoint){
